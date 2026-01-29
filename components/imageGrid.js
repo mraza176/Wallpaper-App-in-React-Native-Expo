3e40311b533cea8ca66236e3a63d44778327cd46
mraza176 <mrazaraheel12@gmail.com>
2026-01-30 02:56:50 +0500

    updated

--- components/imageGrid.js
+++ components/imageGrid.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { MasonryFlashList } from "@shopify/flash-list";
 import ImageCard from "./imageCard";
 import { getColumnsCount, widthPercentage } from "../helpers/common";
